6371c3eba7a4adbec1aeac75202437b265b2ad06
Piket564 <l.iadicicco.0036@vallauri.edu>
2021-08-20 00:08:02 +0200

    Fix
    Fix some error + add support from university

--- background.js
+++ background.js
@@ -1,37 +1,74 @@
 // Message listener
-chrome.browserAction.onClicked.addListener(function (tab) {
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		if(tabs[0].url.indexOf("panopto.com") > 1 || tabs[0].url.indexOf("panopto.eu") > 1) {
-			if (tabs[0].url.indexOf("Embed.aspx") > 1) {
-                var newurl = tabs[0].url.replace('/Embed.aspx','/Viewer.aspx');
-                chrome.tabs.update(tabs[0].id,{url:newurl});
-        chrome.tabs.onUpdated.addListener(function listener (tabId, info) {
-                if (info.status === 'complete' && tabId === tabs[0].id) {
-                    chrome.tabs.onUpdated.removeListener(listener);
-            chrome.tabs.sendMessage(tabs[0].id, {action: "download_video"}, downloadCallback);
-                }
-            });
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        if (tabs[0].url.indexOf("panopto.com") > 1 || tabs[0].url.indexOf("panopto.eu") > 1) {
+            if (tabs[0].url.indexOf("Embed.aspx") > 1) {
+                var newurl = tabs[0].url.replace('/Embed.aspx', '/Viewer.aspx');
+                chrome.tabs.update(tabs[0].id, { url: newurl });
+                chrome.tabs.onUpdated.addListener(function listener(tabId, info) {
+                    if (info.status === 'complete' && tabId === tabs[0].id) {
+                        chrome.tabs.onUpdated.removeListener(listener);
+                        chrome.tabs.sendMessage(tabs[0].id, { action: "download_video" }, downloadCallback);
+                    }
+                });
+            } else {
+                chrome.tabs.sendMessage(tabs[0].id, { action: "download_video" }, downloadCallback);
             }
-		else{
-		chrome.tabs.sendMessage(tabs[0].id, {action: "download_video"}, downloadCallback);
-		}  
-		} else if (tabs[0].url.indexOf("panopto.com") == -1 || tabs[0].url.indexOf("panopto.eu") == -1) {
-			alert("Sorry, but I didn't find any videos. Are you sure the address in the search bar is with \"panopto.eu\" or \"panopto.com\" ?");
-		}
-	})
+        } else if (tabs[0].url.indexOf("panopto.com") == -1 || tabs[0].url.indexOf("panopto.eu") == -1) {
+            alert("Sorry, but I didn't find any videos. Are you sure the address in the search bar is with \"panopto.eu\" or \"panopto.com\" ?");
+        }
+    })
 });
 
 // Callback to download video
 function downloadCallback(infoArray) {
-	if(typeof infoArray != "undefined") {
-			
-		var url = infoArray[0];
-		var name = infoArray[1];
+    if (typeof infoArray != "undefined") {
 
-		chrome.downloads.download( {
-					url: url,
-					filename: name,
-					saveAs: true
-		});
-	}
+        var url = infoArray[0];
+        var name = infoArray[1];
+
+        // chrome.downloads.download({
+        //     url: url,
+        //     filename: name,
+        //     saveAs: true
+        // });
+        //Piket564: Some error with the window/ios downloader, do something with copy/paste
+
+        copyTextToClipboard(infoArray[0] + infoArray[1])
+        alert('Paste your download link in a browser.');
+
+
+    } else {
+        alert('Video Info\'s error!');
+    }
+}
+
+
+//Piket564
+//From https://stackoverflow.com/questions/3436102/copy-to-clipboard-in-chrome-extension
+
+function copyTextToClipboard(text) {
+    //Create a textbox field where we can insert text to. 
+    var copyFrom = document.createElement("textarea");
+
+    //Set the text content to be the text you wished to copy.
+    copyFrom.textContent = text;
+
+    //Append the textbox field into the body as a child. 
+    //"execCommand()" only works when there exists selected text, and the text is inside 
+    //document.body (meaning the text is part of a valid rendered HTML element).
+    document.body.appendChild(copyFrom);
+
+    //Select all the text!
+    copyFrom.select();
+
+    //Execute command
+    document.execCommand('copy');
+
+    //(Optional) De-select the text using blur(). 
+    copyFrom.blur();
+
+    //Remove the textbox field from the document.body, so no other JavaScript nor 
+    //other elements can get access to this.
+    document.body.removeChild(copyFrom);
 }
--- payload.js
+++ payload.js
@@ -1,34 +1,51 @@
 chrome.extension.onMessage.addListener(function(msg, _sender, sendResponse) {
 
- 	if (msg.action == 'download_video') {	
-	// Default parameters to grab video info
-	var name = "";
-	var url = "";
-	var metas = document.getElementsByTagName('meta'); 
-	
-	for (var i=0; i<metas.length; i++) 
-		if (metas[i].getAttribute("name") == "twitter:player:stream") { 
-			console.log(metas[i].getAttribute("content"));
-			url = (metas[i].getAttribute("content").split('?')[0]);
-		} 
+    if (msg.action == 'download_video') {
+        // Default parameters to grab video info
+        let name = "";
+        let url = "";
+        let id = "";
+        let metas = document.getElementsByTagName('meta');
+        for (var i = 0; i < metas.length; i++) {
+            //Piket564 idk but i think that this is unnecessary
+            //
+            // if (metas[i].getAttribute("name") == "twitter:player:stream") {
+            //     console.log(metas[i].getAttribute("content"));
+            //     url = (metas[i].getAttribute("content").split('?')[0]);
+            // } else if (metas[i].getAttribute("property") == "og:title") {
+            //     console.log(metas[i].getAttribute("content"));
+            //     name = metas[i].getAttribute("content").split('?')[0];
+            // }
 
-		else if (metas[i].getAttribute("property") == "og:title") {
-			console.log(metas[i].getAttribute("content"));
-			name = metas[i].getAttribute("content").split('?')[0];
-		}
+            // Search og:url with file_id hoping that Panopto don't change that in the future
+            if (metas[i].getAttribute('property') == 'og:url') {
+                id = metas[i].getAttribute("content").split('?')[1].replace('id=', '');
+                console.log('File ID: ' + id);
+            }
+        }
 
-			// Split the name up (remove invalid characters in file names) && add file format
-			name = name.split('/').join('_'); // Remove slashes
-			name = name.split(' ').join('_'); // Remove spaces
-			name = name.split(':').join('-'); // Remove colons
-			name = name + ".mp4";
 
-			// Pack into array to send through callback function
-			var infoArray = [url, name];
+        // Piket_564 -> Url from Twitter Card and name = file_id
+        name = id;
+        // This download url is from Tw Card
+        url = 'https://pro.panopto.com/Panopto/Podcast/Download/';
 
-			// Callback function
-			sendResponse(infoArray);
 
-  	}
+        // Split the name up (remove invalid characters in file names) && add file format
+        name = name.split('/').join('_'); // Remove slashes
+        name = name.split(' ').join('_'); // Remove spaces
+        name = name.split(':').join('-'); // Remove colons
+        name = name + ".mp4";
+        // Some console.log to provide some basic debugging tool
+        console.log('Name: ' + name);
+        console.log('Direct download: ' + url + name);
+
+        // Pack into array to send through callback function
+        var infoArray = [url, name];
+
+        // Callback function
+        sendResponse(infoArray);
+
+    }
 
 });
